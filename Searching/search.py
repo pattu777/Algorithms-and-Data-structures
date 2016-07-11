f3584d3b489c78dbef33e1e2b0a3b50fa16bddc1
Chinmaya <patanaikchinmaya@gmail.com>
2016-07-11 23:44:57 +0530

    Recursive binary search

--- Searching/search.py
+++ Searching/search.py
@@ -39,31 +39,51 @@ class SearchingAlgos(object):
                     return True
             return False
 
-    def recursive_binary_search(self, var):
+    def recursive_binary_search(self, low, high, var):
         """
         Time Complexity - O(n logn)
         Space Complexity - O(1)
         """
-        pass
+        mid = low + (high-low)/2
+        if low > high:
+            return False
+        elif var < self.arr[mid]:
+            self.recursive_binary_search(low, mid-1, var)
+        elif var > self.arr[mid]:
+            self.recursive_binary_search(mid+1, high, var)
+        else:
+            return True
+        
 
 class SearchingTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(SearchingTest, self).__init__(*args, **kwargs)
         self.test_arr = [x for x in xrange(100)]
 
+    def test_linear_search(self):
+        """Unit tests for liner search."""
+        var = SearchingAlgos(arr=self.test_arr)
+        self.assertTrue(var.linear_search(50))
+        self.assertFalse(var.linear_search(181))
+        self.assertFalse(var.iterative_binary_search(-1231))
+        self.assertTrue(var.iterative_binary_search(79))
+
     def test_iterative_binary_search(self):
+        """Unit test for iterative binary search."""
         var = SearchingAlgos(arr=self.test_arr)
         self.assertTrue(var.iterative_binary_search(50))
         self.assertFalse(var.iterative_binary_search(181))
         self.assertFalse(var.iterative_binary_search(-1231))
         self.assertTrue(var.iterative_binary_search(79))
 
-    def test_linear_search(self):
+    def test_recursive_binary_search(self):
+        """Unit tests for recursive binary search."""
         var = SearchingAlgos(arr=self.test_arr)
-        self.assertTrue(var.linear_search(50))
-        self.assertFalse(var.linear_search(181))
-        self.assertFalse(var.iterative_binary_search(-1231))
-        self.assertTrue(var.iterative_binary_search(79))
+        self.assertTrue(var.recursive_binary_search(0, len(self.test_arr)-1, 50))
+        self.assertFalse(var.recursive_binary_search(0, len(self.test_arr)-1, 181))
+        self.assertFalse(var.recursive_binary_search(0, len(self.test_arr)-1, -1231))
+        self.assertTrue(var.recursive_binary_search(0, len(self.test_arr)-1, 79))
+
 
 if __name__ == '__main__':
     unittest.main()
