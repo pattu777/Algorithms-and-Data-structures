a0c8b453d5614d42c0b1ac93ebd7e159adc499b8
Chinmaya <patanaikchinmaya@gmail.com>
2016-07-17 01:04:22 +0530

    unit tests for Trie

--- Trie/tests.py
+++ Trie/tests.py
@@ -1,11 +1,18 @@
 import unittest
 
-from .trie import Node, Trie
+from trie import Trie
 
 class MyTest(unittest.TestCase):
     def test_trie(self):
-        pass
+        tr = Trie()
+        words = ["Hello World", "Chinmaya", "Strange", "Things", "Will"]
+        for x in words:
+            tr.insert(x)
 
+        self.assertTrue(tr.search("Hello World"))
+        self.assertFalse(tr.search("Hii"))
+        self.assertTrue(tr.search("Chinmaya"))
+        self.assertTrue("Will")
 
 if __name__ == '__main__':
     unittest.main()
